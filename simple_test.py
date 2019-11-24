a66df7318abfbef1b666591ecc7a27cfb2832e11
Yesid Oc <32651875+yaochoal@users.noreply.github.com>
2019-11-23 19:22:22 -0500

    Add files via upload

---
+++ simple_test.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import gym
+import argparse
+import ns3gym
+
+env = gym.make('ns3-v0')
+env.reset()
+
+ob_space = env.observation_space
+ac_space = env.action_space
+print("Observation space: ", ob_space,  ob_space.dtype)
+print("Action space: ", ac_space, ac_space.dtype)
+
+stepIdx = 0
+
+try:
+    obs = env.reset()
+    print("Step: ", stepIdx)
+    print("---obs: ", obs)
+
+    while True:
+        stepIdx += 1
+
+        action = env.action_space.sample()
+        print("---action: ", action)
+        obs, reward, done, info = env.step(action)
+
+        print("Step: ", stepIdx)
+        print("---obs, reward, done, info: ", obs, reward, done, info)
+
+        if done:
+            break
+
+except KeyboardInterrupt:
+    print("Ctrl-C -> Exit")
+finally:
+    env.close()
+    print("Done")
